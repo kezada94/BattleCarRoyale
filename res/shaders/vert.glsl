87d947df66a060b222fbfb4932caa94578eed62f
Felipe Quezada <kezada66@gmail.com>
2017-09-10 21:17:02 -0300

    hola

--- res/shaders/vert.glsl
+++ res/shaders/vert.glsl
@@ -1,12 +1,12 @@
 #version 410
 
-//layout(location = 0) in vec3 vertex_position;
-//layout(location = 1) in vec3 vertex_normal;
-//layout(location = 2) in vec2 texture_coord;
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_normal;
+layout(location = 2) in vec2 texture_coord;
 
-in vec3 vertex_position;
-in vec3 vertex_normal;
-in vec2 texture_coord;
+//in vec3 vertex_position;
+//in vec3 vertex_normal;
+//in vec2 texture_coord;
 
 uniform mat4 view, proj;
 
