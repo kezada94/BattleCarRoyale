2d233a6ad8b9c3eaf3c8fe9be2123cecbf22e119
Felipe Quezada <kezada66@gmail.com>
2017-10-22 18:19:08 -0300

    helee

---
+++ res/shaders/vert_debug.glsl
@@ -0,0 +1,10 @@
+#version 410 core
+layout (location = 0) in vec3 position;
+
+uniform mat4 projection;
+uniform mat4 view;
+
+void main()
+{
+    gl_Position = projection * view * vec4(position, 1.0f);
+}
